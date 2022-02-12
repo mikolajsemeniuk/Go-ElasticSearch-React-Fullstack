4f8fdd7254f61bdaa9c89aeb23579ddbf4cb5f04
mikolaj semeniuk <m.semeniuk@icloud.com>
2022-02-12 14:52:02 +0100

    refactoring logger

--- extensions/logger.go
+++ extensions/logger.go
@@ -1,28 +1,38 @@
 package extensions
 
 import (
+	"os"
+
 	"github.com/TwiN/go-color"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var logger *zap.Logger
+var (
+	logger *zap.Logger
+	err    error
+)
 
 func init() {
 	encoderConfiguration := zap.NewDevelopmentEncoderConfig()
+	configuration := zap.NewDevelopmentConfig()
+
+	environment := os.Getenv("ENVIROMENT")
+	if environment != "development" {
+		encoderConfiguration = zap.NewProductionEncoderConfig()
+		configuration = zap.NewProductionConfig()
+	}
+
 	encoderConfiguration.TimeKey = "timestamp"
 	encoderConfiguration.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfiguration.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
-	configuration := zap.NewDevelopmentConfig()
 	configuration.EncoderConfig = encoderConfiguration
 
-	var err error
 	logger, err = configuration.Build(zap.AddCallerSkip(1))
 	if err != nil {
 		panic(err)
 	}
-
 }
 
 func Info(message string, fields ...zap.Field) {
